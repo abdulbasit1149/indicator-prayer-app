320fb8418b1a7d3e7ff5566fac2780c27d9ae9da
abdulbasit <brcoopbasit@gmail.com>
2017-07-13 21:46:37 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+import os
+import signal
+import json
+import gi
+
+from urllib.request import Request, urlopen, URLError
+gi.require_version('AppIndicator3', '0.1')
+gi.require_version('Gtk', '3.0')
+gi.require_version('Notify', '0.7')
+from gi.repository import Gtk as gtk
+from gi.repository import AppIndicator3 as appindicator
+from gi.repository import Notify as notify
+
+
+APPINDICATOR_ID = 'myappindicator'
+
+def main():
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('sample_icon.svg'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+    indicator.set_menu(build_menu())
+    notify.init(APPINDICATOR_ID)
+    gtk.main()
+    print("hello world")
+
+def build_menu():
+    menu = gtk.Menu()
+    item_joke = gtk.MenuItem('Joke')
+    item_joke.connect('activate', joke)
+    menu.append(item_joke)
+    item_quit = gtk.MenuItem('Quit')
+    item_quit.connect('activate', quit)
+    menu.append(item_quit)
+    menu.show_all()
+    return menu
+
+def fetch_joke():
+    request = Request('http://api.icndb.com/jokes/random?limitTo=[nerdy]')
+    response = urlopen(request)
+    joke = json.loads(response.read().decode("utf-8"))['value']['joke']
+    return joke
+ 
+def joke(_):
+    notify.Notification.new("<b>Joke</b>", fetch_joke(), None).show()
+
+def quit(_):
+    notify.uninit()
+    gtk.main_quit()
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    main()
