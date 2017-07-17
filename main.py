5d9ffc5ebe1ba4cbc6d999f04a8deb0a0eaa2939
abdulbasit <brcoopbasit@gmail.com>
2017-07-16 20:31:08 -0400

    finish beta prayer app

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 import signal
 import json
 import gi
+import time
+import requests
+from datetime import datetime
 
 from urllib.request import Request, urlopen, URLError
 gi.require_version('AppIndicator3', '0.1')
@@ -13,40 +16,52 @@ from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 from gi.repository import Notify as notify
 
+prayers = ["Fajr", "Dhuhr", "Asr", "Maghrib", "Isha"]
 
 APPINDICATOR_ID = 'myappindicator'
 
 def main():
     indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('sample_icon.svg'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-    indicator.set_menu(build_menu())
-    notify.init(APPINDICATOR_ID)
+    indicator.set_menu(prayerTimings())
     gtk.main()
-    print("hello world")
 
-def build_menu():
+def prayerTimings():
     menu = gtk.Menu()
-    item_joke = gtk.MenuItem('Joke')
-    item_joke.connect('activate', joke)
-    menu.append(item_joke)
-    item_quit = gtk.MenuItem('Quit')
-    item_quit.connect('activate', quit)
-    menu.append(item_quit)
+    timings = fetch_prayersTimings()
+    for prayer in prayers:
+        time = timings[prayer]
+        layout = "{0} {1}"
+        layout = layout.format(prayer,time)
+        item1 = gtk.MenuItem(layout)
+        menu.append(item1)
     menu.show_all()
     return menu
 
-def fetch_joke():
-    request = Request('http://api.icndb.com/jokes/random?limitTo=[nerdy]')
-    response = urlopen(request)
-    joke = json.loads(response.read().decode("utf-8"))['value']['joke']
-    return joke
- 
-def joke(_):
-    notify.Notification.new("<b>Joke</b>", fetch_joke(), None).show()
+def get_longtitudeLatitude():
+    url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    params = {'sensor': 'false', 'address': 'Milton, ON'}
+    r = requests.get(url, params=params)
+    results = r.json()['results']
+    location = results[0]['geometry']['location']
+    return location['lat'], location['lng']
 
-def quit(_):
-    notify.uninit()
-    gtk.main_quit()
+def fetch_prayersTimings():
+    lat, lng = get_longtitudeLatitude()
+    timeStamp = str(int(time.time()))
+    base = 'http://api.aladhan.com/timings/{0}?latitude={1}&longitude={2}&method2'
+    base = base.format(timeStamp, lat, lng)
+
+    request = Request(base)
+    response = urlopen(request)
+    prayersTimings = json.loads(response.read().decode("utf-8"))["data"]["timings"]
+
+    timings = {}
+    for timingName in prayersTimings:
+        if timingName in prayers:
+            timeToDisplay = datetime.strptime(prayersTimings[timingName], "%H:%M")
+            timings[timingName] = timeToDisplay.strftime("%I:%M %p")
+    return timings
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal.SIG_DFL)
